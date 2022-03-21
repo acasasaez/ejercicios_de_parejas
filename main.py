0560f938b377dbf902a978cee26de9053bf79ce0
acasasaez <acasasae@myuax.com>
2022-03-21 22:57:22 +0100

    termino el primer ejemplo en palindromos() -- main

--- main.py
+++ main.py
@@ -16,7 +16,11 @@ def palindromos():
           Fore.YELLOW + mi_cadena + Fore.WHITE)
     mostrar_resultado("Frase normalizada (" + # MOSTRAR RESULTADO ESTARA EN EL HELPERS
                       str(len(mi_cadena.normalized_str)) + ") ",  mi_cadena.normalized_str)
-    
+    mostrar_resultado("Es palindromo (iterativo)",
+                      mi_cadena.is_palindrome_iterative())
+    mostrar_resultado("Es palindromo (recursivo)",
+                      mi_cadena.is_palindrome_recursive())
+    print()   
 
 ###
 # I N I C I O   P R O G R A M A
